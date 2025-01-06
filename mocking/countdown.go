eb7a4e3561d0766793a57d6f44ef309bc305b261
Kirill Mironov <kirill.mironov@gmail.com>
2025-01-06 22:59:51 +0300

    mocking

---
+++ mocking/countdown.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, "Go!")
+}
